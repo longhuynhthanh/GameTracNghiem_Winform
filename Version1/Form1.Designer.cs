8316c10c18b722169b87fc9e3d8cfc891976c0ec
LongHT <38778007+longhuynhthanh@users.noreply.github.com>
2019-05-31 21:15:18 +0700

    upload source

---
+++ Version1/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Version1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gbCauHoi = new System.Windows.Forms.GroupBox();
+            this.txtCauHoi = new System.Windows.Forms.TextBox();
+            this.gbDapAn = new System.Windows.Forms.GroupBox();
+            this.rdbD = new System.Windows.Forms.RadioButton();
+            this.rdbC = new System.Windows.Forms.RadioButton();
+            this.rdbB = new System.Windows.Forms.RadioButton();
+            this.rdbA = new System.Windows.Forms.RadioButton();
+            this.txtD = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.tmThoiGian = new System.Windows.Forms.Timer(this.components);
+            this.lblDongHo = new System.Windows.Forms.Label();
+            this.tmDem = new System.Windows.Forms.Timer(this.components);
+            this.lblDem = new System.Windows.Forms.Label();
+            this.gbCauHoi.SuspendLayout();
+            this.gbDapAn.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbCauHoi
+            // 
+            this.gbCauHoi.Controls.Add(this.txtCauHoi);
+            this.gbCauHoi.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbCauHoi.Location = new System.Drawing.Point(114, 60);
+            this.gbCauHoi.Name = "gbCauHoi";
+            this.gbCauHoi.Size = new System.Drawing.Size(503, 147);
+            this.gbCauHoi.TabIndex = 0;
+            this.gbCauHoi.TabStop = false;
+            this.gbCauHoi.Text = "Câu Hỏi";
+            // 
+            // txtCauHoi
+            // 
+            this.txtCauHoi.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCauHoi.Location = new System.Drawing.Point(6, 19);
+            this.txtCauHoi.Multiline = true;
+            this.txtCauHoi.Name = "txtCauHoi";
+            this.txtCauHoi.ReadOnly = true;
+            this.txtCauHoi.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtCauHoi.Size = new System.Drawing.Size(497, 122);
+            this.txtCauHoi.TabIndex = 0;
+            // 
+            // gbDapAn
+            // 
+            this.gbDapAn.Controls.Add(this.rdbD);
+            this.gbDapAn.Controls.Add(this.rdbC);
+            this.gbDapAn.Controls.Add(this.rdbB);
+            this.gbDapAn.Controls.Add(this.rdbA);
+            this.gbDapAn.Controls.Add(this.txtD);
+            this.gbDapAn.Controls.Add(this.txtC);
+            this.gbDapAn.Controls.Add(this.txtB);
+            this.gbDapAn.Controls.Add(this.txtA);
+            this.gbDapAn.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbDapAn.Location = new System.Drawing.Point(120, 213);
+            this.gbDapAn.Name = "gbDapAn";
+            this.gbDapAn.Size = new System.Drawing.Size(497, 289);
+            this.gbDapAn.TabIndex = 1;
+            this.gbDapAn.TabStop = false;
+            this.gbDapAn.Text = "Đáp Án";
+            // 
+            // rdbD
+            // 
+            this.rdbD.AutoSize = true;
+            this.rdbD.Location = new System.Drawing.Point(6, 230);
+            this.rdbD.Name = "rdbD";
+            this.rdbD.Size = new System.Drawing.Size(42, 26);
+            this.rdbD.TabIndex = 11;
+            this.rdbD.TabStop = true;
+            this.rdbD.Text = "D";
+            this.rdbD.UseVisualStyleBackColor = true;
+            // 
+            // rdbC
+            // 
+            this.rdbC.AutoSize = true;
+            this.rdbC.Location = new System.Drawing.Point(6, 174);
+            this.rdbC.Name = "rdbC";
+            this.rdbC.Size = new System.Drawing.Size(42, 26);
+            this.rdbC.TabIndex = 10;
+            this.rdbC.TabStop = true;
+            this.rdbC.Text = "C";
+            this.rdbC.UseVisualStyleBackColor = true;
+            // 
+            // rdbB
+            // 
+            this.rdbB.AutoSize = true;
+            this.rdbB.Location = new System.Drawing.Point(6, 111);
+            this.rdbB.Name = "rdbB";
+            this.rdbB.Size = new System.Drawing.Size(41, 26);
+            this.rdbB.TabIndex = 9;
+            this.rdbB.TabStop = true;
+            this.rdbB.Text = "B";
+            this.rdbB.UseVisualStyleBackColor = true;
+            // 
+            // rdbA
+            // 
+            this.rdbA.AutoSize = true;
+            this.rdbA.Location = new System.Drawing.Point(6, 45);
+            this.rdbA.Name = "rdbA";
+            this.rdbA.Size = new System.Drawing.Size(41, 26);
+            this.rdbA.TabIndex = 8;
+            this.rdbA.TabStop = true;
+            this.rdbA.Text = "A";
+            this.rdbA.UseVisualStyleBackColor = true;
+            // 
+            // txtD
+            // 
+            this.txtD.Location = new System.Drawing.Point(45, 216);
+            this.txtD.Multiline = true;
+            this.txtD.Name = "txtD";
+            this.txtD.ReadOnly = true;
+            this.txtD.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtD.Size = new System.Drawing.Size(446, 56);
+            this.txtD.TabIndex = 7;
+            this.txtD.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(45, 152);
+            this.txtC.Multiline = true;
+            this.txtC.Name = "txtC";
+            this.txtC.ReadOnly = true;
+            this.txtC.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtC.Size = new System.Drawing.Size(446, 58);
+            this.txtC.TabIndex = 6;
+            this.txtC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(45, 87);
+            this.txtB.Multiline = true;
+            this.txtB.Name = "txtB";
+            this.txtB.ReadOnly = true;
+            this.txtB.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtB.Size = new System.Drawing.Size(446, 59);
+            this.txtB.TabIndex = 5;
+            this.txtB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(45, 26);
+            this.txtA.Multiline = true;
+            this.txtA.Name = "txtA";
+            this.txtA.ReadOnly = true;
+            this.txtA.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtA.Size = new System.Drawing.Size(446, 55);
+            this.txtA.TabIndex = 4;
+            this.txtA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tmThoiGian
+            // 
+            this.tmThoiGian.Interval = 1000;
+            this.tmThoiGian.Tick += new System.EventHandler(this.tmThoiGian_Tick);
+            // 
+            // lblDongHo
+            // 
+            this.lblDongHo.AutoSize = true;
+            this.lblDongHo.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDongHo.Location = new System.Drawing.Point(27, 60);
+            this.lblDongHo.Name = "lblDongHo";
+            this.lblDongHo.Size = new System.Drawing.Size(55, 22);
+            this.lblDongHo.TabIndex = 2;
+            this.lblDongHo.Text = "00:00";
+            // 
+            // tmDem
+            // 
+            this.tmDem.Interval = 1000;
+            this.tmDem.Tick += new System.EventHandler(this.tmDem_Tick);
+            // 
+            // lblDem
+            // 
+            this.lblDem.AutoSize = true;
+            this.lblDem.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDem.Location = new System.Drawing.Point(309, 35);
+            this.lblDem.Name = "lblDem";
+            this.lblDem.Size = new System.Drawing.Size(30, 22);
+            this.lblDem.TabIndex = 3;
+            this.lblDem.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 514);
+            this.Controls.Add(this.lblDem);
+            this.Controls.Add(this.lblDongHo);
+            this.Controls.Add(this.gbDapAn);
+            this.Controls.Add(this.gbCauHoi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbCauHoi.ResumeLayout(false);
+            this.gbCauHoi.PerformLayout();
+            this.gbDapAn.ResumeLayout(false);
+            this.gbDapAn.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbCauHoi;
+        private System.Windows.Forms.TextBox txtCauHoi;
+        private System.Windows.Forms.GroupBox gbDapAn;
+        private System.Windows.Forms.TextBox txtD;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.RadioButton rdbD;
+        private System.Windows.Forms.RadioButton rdbC;
+        private System.Windows.Forms.RadioButton rdbB;
+        private System.Windows.Forms.RadioButton rdbA;
+        private System.Windows.Forms.Timer tmThoiGian;
+        private System.Windows.Forms.Label lblDongHo;
+        private System.Windows.Forms.Timer tmDem;
+        private System.Windows.Forms.Label lblDem;
+    }
+}
+
---
+++ Version1/Form1.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Version1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[,] DanhSachCauHoi = new string[,] {
+            {"Long có đẹp trai không?", "Rất đẹp trai", "Quá đẹp trai", "Chuẩn quá anh ơi", "Siêu Đẹp"},
+            {"1 + 1 = ?", "2", "3", "4", "5"},
+            {"Bạn có thấy mình giống chó không?", "Không", "Có", "Quá Giống", "Rất Giống"},
+            {"Bạn làm gì khi vào nhà tắm?", "Đi ỉa", "Đi Đái", "Quay Tay", "Làm Việc Khác" },
+            {"Nói Thiệt đi bạn đã xem phim con heo lần nào chưa?", "Mình còn nhỏ chưa xem bao giờ", "Mình thanh niên cứng", "Thằng nào chả xem", "Phim con heo là gì?" },
+                                              };
+        string[] DapAn = new string[] { "Quá đẹp trai", "3", "Quá Giống", "Quay Tay", "Mình còn nhỏ chưa xem bao giờ" };
+        string[] DapAnNguoiChoiChon;
+        int cauhoi;
+        RadioButton[] DanhSachCacRadioButton;
+        PictureBox ptr;
+        Random rd;
+        ToolTip tl;
+        int giay = 0, phut = 0;
+        bool KiemTraDaTraLoiCauHoi = false;
+        DialogResult traloi;
+        DialogResult ThongBaohetGio;
+        bool KiemTraThongBaoHetGio = false;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // start đồng hồ đếm thời gian làm cả 5 câu trắc nghiệm.
+            tmThoiGian.Start();
+            // start đồng hồ giới hạn thời gian làm một câu
+            tmDem.Start();
+
+
+
+            tl = new ToolTip();
+            rd = new Random();
+            ptr = new PictureBox();
+            ptr.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptr.Size = new Size(767, 552);
+            this.Controls.Add(ptr);
+            ptr.Click += Ptr_Click;
+            ptr.Visible = false;
+
+            cauhoi = 1;
+            LoadCauHoi(cauhoi);
+            DapAnNguoiChoiChon = new string[DanhSachCauHoi.GetLength(0)];
+            //Tạo tự động các radio button câu hỏi đã trả lời, khi click trả lời đúng câu hỏi thì sẽ hiện ra và 
+            //click vào sẽ xem lại được câu đã trả lời
+            DanhSachCacRadioButton = new RadioButton[DanhSachCauHoi.GetLength(0)];
+            for (int i = 0; i < DanhSachCacRadioButton.Count(); ++i)
+            {
+                DanhSachCacRadioButton[i] = new RadioButton();
+                DanhSachCacRadioButton[i].Location = new Point(gbCauHoi.Location.X + gbCauHoi.Size.Width + 20, gbCauHoi.Location.Y + 30 * i);
+                DanhSachCacRadioButton[i].Text = "Câu Hỏi " + (i + 1);
+                DanhSachCacRadioButton[i].Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                DanhSachCacRadioButton[i].AutoSize = true;
+                DanhSachCacRadioButton[i].Enabled = false;
+                DanhSachCacRadioButton[i].Tag = i + 1;
+                this.Controls.Add(DanhSachCacRadioButton[i]);
+                DanhSachCacRadioButton[i].CheckedChanged += DanhSachCacRadioButton_CheckedChanged;
+            }
+
+
+            foreach (Control c in this.gbDapAn.Controls)
+            {
+                if (c is RadioButton)
+                {
+                    RadioButton rd = (RadioButton)c;
+                    rd.CheckedChanged += KiemTraDapAn_CheckedChanged;
+                }
+            }
+            foreach (Control c in this.gbDapAn.Controls)
+            {
+                if (c is TextBox)
+                {
+                    c.Click += KiemTraDapAn_Click;
+                }
+            }
+        }
+        private void DanhSachCacRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rd = (RadioButton)sender;
+            if (rd.Checked)
+            {
+                LoadCauHoi((int)rd.Tag);
+
+                // Load đáp án đúng
+                if (cauhoi < (int)rd.Tag || this.gbDapAn.Enabled == false)
+                {
+                    if (txtA.Text == DapAn[(int)rd.Tag - 1])
+                    {
+                        txtA.BackColor = Color.Red;
+                        txtA.ForeColor = Color.White;
+                    }
+                    else if (txtB.Text == DapAn[(int)rd.Tag - 1])
+                    {
+                        txtB.BackColor = Color.Red;
+                        txtB.ForeColor = Color.White;
+                    }
+                    else if (txtC.Text == DapAn[(int)rd.Tag - 1])
+                    {
+                        txtC.BackColor = Color.Red;
+                        txtC.ForeColor = Color.White;
+                    }
+                    else if (txtD.Text == DapAn[(int)rd.Tag - 1])
+                    {
+                        txtD.BackColor = Color.Red;
+                        txtD.ForeColor = Color.White;
+                    }
+                }
+
+                // Load đáp án của người chơi chọn
+                if (txtA.Text == DapAnNguoiChoiChon[(int)rd.Tag - 1])
+                {
+                    txtA.BackColor = Color.Blue;
+                    txtA.ForeColor = Color.White;
+                }
+                else if (txtB.Text == DapAnNguoiChoiChon[(int)rd.Tag - 1])
+                {
+                    txtB.BackColor = Color.Blue;
+                    txtB.ForeColor = Color.White;
+                }
+                else if (txtC.Text == DapAnNguoiChoiChon[(int)rd.Tag - 1])
+                {
+                    txtC.BackColor = Color.Blue;
+                    txtC.ForeColor = Color.White;
+                }
+                else if (txtD.Text == DapAnNguoiChoiChon[(int)rd.Tag - 1])
+                {
+                    txtD.BackColor = Color.Blue;
+                    txtD.ForeColor = Color.White;
+                }
+                this.gbDapAn.Enabled = false;
+                if (rd.Checked == DanhSachCacRadioButton[cauhoi - 1].Checked)
+                {
+                    this.gbDapAn.Enabled = true;
+                }
+            }
+        }
+
+        private void LoadCauHoi(int cauhoi)
+        {
+            //KiemTraDaTraLoiCauHoi = false;
+
+            this.gbDapAn.Enabled = true;
+            txtA.BackColor = txtB.BackColor = txtC.BackColor = txtD.BackColor = Color.White;
+            txtA.ForeColor = txtB.ForeColor = txtC.ForeColor = txtD.ForeColor = Color.Black;
+
+            // Reset lại các radio button
+            rdbA.Checked = rdbB.Checked = rdbC.Checked = rdbD.Checked = false;
+            //foreach (Control c in this.Controls)
+            //{
+            //    if (c is RadioButton)
+            //    {
+            //        RadioButton rd = (RadioButton)c;
+            //        rd.Checked = false;
+            //    }
+            //}
+
+            // Hiện lại radio button câu hỏi đã trả lời đúng
+            if (cauhoi >= 2)
+            {
+                DanhSachCacRadioButton[cauhoi - 2].Enabled = true;
+                DanhSachCacRadioButton[cauhoi - 1].Enabled = true;
+
+                DanhSachCacRadioButton[cauhoi - 1].Checked = true;
+            }
+            txtCauHoi.Text = DanhSachCauHoi[cauhoi - 1, 0];
+            txtA.Text = DanhSachCauHoi[cauhoi - 1, 1];
+            txtB.Text = DanhSachCauHoi[cauhoi - 1, 2];
+            txtC.Text = DanhSachCauHoi[cauhoi - 1, 3];
+            txtD.Text = DanhSachCauHoi[cauhoi - 1, 4];
+        }
+
+        private void KiemTraDapAn_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rd = (RadioButton)sender;
+            if (rd.Checked)
+            {
+                txtA.BackColor = txtB.BackColor = txtC.BackColor = txtD.BackColor = Color.White;
+                txtA.ForeColor = txtB.ForeColor = txtC.ForeColor = txtD.ForeColor = Color.Black;
+                string s = "";
+                if (rdbA.Checked)
+                {
+                    txtA.BackColor = Color.Blue;
+                    txtA.ForeColor = Color.White;
+                    s = txtA.Text;
+                }
+                else if (rdbB.Checked)
+                {
+                    txtB.BackColor = Color.Blue;
+                    txtB.ForeColor = Color.White;
+                    s = txtB.Text;
+                }
+                else if (rdbC.Checked)
+                {
+                    txtC.BackColor = Color.Blue;
+                    txtC.ForeColor = Color.White;
+                    s = txtC.Text;
+                }
+                else if (rdbD.Checked)
+                {
+                    txtD.BackColor = Color.Blue;
+                    txtD.ForeColor = Color.White;
+                    s = txtD.Text;
+                }
+                DapAnNguoiChoiChon[cauhoi - 1] = s;
+                traloi = MessageBox.Show("Bạn có chắc chắn đây là Đáp Án cuối cùng?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (traloi == DialogResult.Yes)
+                {
+                    KiemTraDaTraLoiCauHoi = true;
+                    if (s == DapAn[cauhoi - 1])
+                    {
+                        MessageBox.Show("Bạn trả lời chuẩn quá");
+                        // Đừng thời gian đếm lại.
+                        tmDem.Stop();
+                        dem = 0;
+                        lblDem.Text = "00";
+
+                        LoadHinhAnh();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sai Rồi!!!!! Ngu như chó");
+                        tmDem.Stop();
+                        tmThoiGian.Stop();
+                        MessageBox.Show("Bạn đã chơi hết " + lblDongHo.Text + "s cảm ơn bạn đã chơi");
+                        foreach (Control c in this.gbDapAn.Controls)
+                        {
+                            if (c is TextBox)
+                            {
+                                TextBox tx = (TextBox)c;
+                                if (tx.Text == DapAn[cauhoi - 1])
+                                {
+                                    tx.BackColor = Color.Red;
+                                    tx.ForeColor = Color.White;
+                                }
+                            }
+                        }
+                        this.gbDapAn.Enabled = false;
+                        foreach (Control c in this.Controls)
+                        {
+                            if (c is RadioButton)
+                            {
+                                RadioButton rdb = (RadioButton)c;
+                                rdb.Enabled = false;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Lần sau chắc chắn nha mày");
+                    if (KiemTraThongBaoHetGio == false)
+                    {
+                        rd.Checked = false;
+                        txtA.BackColor = txtB.BackColor = txtC.BackColor = txtD.BackColor = Color.White;
+                        txtA.ForeColor = txtB.ForeColor = txtC.ForeColor = txtD.ForeColor = Color.Black;
+                    }
+                }
+            }
+        }
+
+        private void KiemTraDapAn_Click(object sender, EventArgs e)
+        {
+            TextBox tx = (TextBox)sender;
+            if (tx.Name == "txtA")
+            {
+                rdbA.Checked = true;
+            }
+            else if (tx.Name == "txtB")
+            {
+                rdbB.Checked = true;
+            }
+            else if (tx.Name == "txtC")
+            {
+                rdbC.Checked = true;
+            }
+            else if (tx.Name == "txtD")
+            {
+                rdbD.Checked = true;
+            }
+        }
+        void LoadHinhAnh()
+        {
+            for (int i = 0; i < this.Controls.Count; ++i)
+            {
+                this.Controls[i].Visible = false;
+            }
+            ptr.Visible = true;
+            int index = rd.Next(1, 7);
+            string s = "Hinh Gai Dep/" + index + ".jpg";
+            ptr.Image = Image.FromFile(s);
+            tl.SetToolTip(ptr, "Click vào em để qua câu mới nha !!!!!");
+        }
+
+        private void tmThoiGian_Tick(object sender, EventArgs e)
+        {
+            giay++;
+            if (giay == 60)
+            {
+                phut++;
+                giay = 0;
+            }
+            lblDongHo.Text = phut.ToString().Count() == 1 ? "0" + phut.ToString() : phut.ToString();
+            lblDongHo.Text += ":" + (giay.ToString().Count() == 1 ? "0" + giay.ToString() : giay.ToString());
+        }
+        int dem = 0;
+        private void tmDem_Tick(object sender, EventArgs e)
+        {
+            dem++;
+            lblDem.Text = dem.ToString().Count() == 1 ? "0" + dem.ToString() : dem.ToString();
+            if (dem == 10 && KiemTraDaTraLoiCauHoi == false)
+            {
+                tmDem.Stop();
+                tmThoiGian.Stop();
+                this.gbDapAn.Enabled = false;
+                this.gbCauHoi.Enabled = false;
+
+                ThongBaohetGio =  MessageBox.Show("Đã hết thời gian bạn thua rồi", "Thông Báo", MessageBoxButtons.OK);
+                MessageBox.Show("Bạn đã chơi hết " + lblDongHo.Text + "s cảm ơn bạn đã chơi");
+                KiemTraThongBaoHetGio = true;
+                if (ThongBaohetGio == DialogResult.OK)
+                {
+                    SendKeys.Send("{TAB}");
+                    SendKeys.Send("{ENTER}");
+                    SendKeys.Send("{ENTER}");
+                    foreach (Control c in this.gbDapAn.Controls)
+                    {
+                        if (c is TextBox)
+                        {
+                            TextBox tx = (TextBox)c;
+                            if (tx.Text == DapAn[cauhoi - 1])
+                            {
+                                tx.BackColor = Color.Red;
+                                tx.ForeColor = Color.White;
+                            }
+                        }
+                    }
+                    foreach (Control c in this.Controls)
+                    {
+                        if (c is RadioButton)
+                        {
+                            RadioButton rdb = (RadioButton)c;
+                            rdb.Enabled = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Ptr_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.Controls.Count; ++i)
+            {
+                this.Controls[i].Visible = true;
+            }
+            cauhoi++;
+            if (cauhoi > DanhSachCauHoi.GetLength(0))
+            {
+                tmDem.Stop();
+                tmThoiGian.Stop();
+                ptr.Visible = false;
+                MessageBox.Show("Đã hết câu hỏi, bạn đã chơi hết " + lblDongHo.Text + "s cảm ơn bạn đã chơi");
+                this.gbDapAn.Enabled = false;
+                cauhoi--;
+            }
+            else
+            {
+                ptr.Visible = false;
+
+                // Chạy lại thời gian giới hạn dành cho câu hỏi
+                tmDem.Start();
+
+                LoadCauHoi(cauhoi);
+            }
+        }
+    }
+}
